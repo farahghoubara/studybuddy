75199354f67691202b0ba9d150880a04d58ba08c
unknown <farah.ghoubara9@gmail.com>
2024-05-03 10:57:44 +0300

    create subject seeder

---
+++ database/seeders/SubjectSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Database\Seeder;
+use App\Models\Subject;
+
+class SubjectSeeder extends Seeder
+{
+    public function run()
+    {
+        Subject::create([
+            'name' => 'Software Engineering',
+        ]);
+
+        Subject::create([
+            'name' => 'Algorithms & Data Structures',
+        ]);
+
+        Subject::create([
+            'name' => 'Data Management & Analysis',
+        ]);
+
+        Subject::create([
+            'name' => 'Operating Systems',
+        ]);
+
+        Subject::create([
+            'name' => 'Object Oriented Programming',
+        ]);
+
+        Subject::create([
+            'name' => 'WEb Technologies',
+        ]);
+
+        Subject::create([
+            'name' => 'Machine Learning & Artificial Intelligence',
+        ]);
+
+        Subject::create([
+            'name' => 'Ethics, Law & The Governance in IT',
+        ]);
+
+    }
+}
